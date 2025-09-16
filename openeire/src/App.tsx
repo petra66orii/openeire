f69e7171f5e02a35b311fe825bf647e98598bac5
Petra <petra66orii@gmail.com>
2025-09-16 19:48:04 +0100

    Add routing and AuthProvider, #3

--- openeire/src/App.tsx
+++ openeire/src/App.tsx
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from "./pages/RegisterPage";
 import VerificationStatusPage from "./pages/VerificationStatusPage";
+import LoginPage from "./pages/LoginPage";
 
 const HomePage = () => <div>Home Page</div>;
+const ProfilePage = () => <div>Welcome to your profile!</div>;
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
       <Route
         path="/verify-email/confirm/:token"
         element={<VerificationStatusPage />}
