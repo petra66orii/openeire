9fb3d8cee3b8957e428af24c2688b5f509d262c6
Petra <petra66orii@gmail.com>
2025-09-17 19:56:45 +0100

    Route profile page and protect user profile route, #6

--- openeire/src/App.tsx
+++ openeire/src/App.tsx
@@ -5,6 +5,7 @@ import LoginPage from "./pages/LoginPage";
 import LogoutPage from "./pages/LogoutPage";
 import RequestPasswordResetPage from "./pages/RequestPasswordResetPage";
 import ConfirmPasswordResetPage from "./pages/ConfirmPasswordResetPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const HomePage = () => <div>Home Page</div>;
 const ProfilePage = () => <div>Welcome to your profile!</div>;
@@ -12,11 +13,11 @@ const ProfilePage = () => <div>Welcome to your profile!</div>;
 function App() {
   return (
     <Routes>
+      {/* Public Routes */}
       <Route path="/" element={<HomePage />} />
       <Route path="/register" element={<RegisterPage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/logout" element={<LogoutPage />} />
-      <Route path="/profile" element={<ProfilePage />} />
       <Route
         path="/request-password-reset"
         element={<RequestPasswordResetPage />}
@@ -28,6 +29,16 @@ function App() {
       <Route
         path="/verify-email/confirm/:token"
         element={<VerificationStatusPage />}
+      />
+
+      {/* Protected Routes */}
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
       />
     </Routes>
   );
