6ce3aaffa3f132a1c9c6a66dac804406513ffb1c
Petra <petra66orii@gmail.com>
2025-09-30 18:55:29 +0100

    Implement sections in home page, #22

--- openeire/src/pages/HomePage.tsx
+++ openeire/src/pages/HomePage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import HeroSection from "../components/HeroSection";
+import ServicesSection from "../components/ServicesSection";
+import CertsSection from "../components/CertsSection";
+import TestimonialCarousel from "../components/TestimonialCarousel";
 
 const HomePage: React.FC = () => {
-  return <div>{/* We will build this out in the next steps */}</div>;
+  return (
+    <div>
+      <HeroSection />
+      <ServicesSection />
+      <CertsSection />
+      <TestimonialCarousel />
+    </div>
+  );
 };
 
 export default HomePage;
