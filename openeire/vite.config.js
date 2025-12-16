a3952a6ae881c319a038dee45653a832a7a228d4
Petra <petra66orii@gmail.com>
2025-12-16 21:05:00 +0000

    Add additional settings

--- openeire/vite.config.js
+++ openeire/vite.config.js
@@ -5,4 +5,14 @@ export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
