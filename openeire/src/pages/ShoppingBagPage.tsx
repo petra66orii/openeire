d360f2de0326b8fbadf073116f69471f05cb988f
Petra <petra66orii@gmail.com>
2026-01-13 20:43:01 +0000

    Add related products logic to shoping bag, #32

--- openeire/src/pages/ShoppingBagPage.tsx
+++ openeire/src/pages/ShoppingBagPage.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useCart } from "../context/CartContext";
 import { Link } from "react-router-dom";
 import BagItem from "../components/BagItem";
 import OrderSummary from "../components/OrderSummary";
+import RelatedProducts from "../components/RelatedProducts";
+import { getShoppingBagRecommendations, GalleryItem } from "../services/api";
 
 const ShoppingBagPage: React.FC = () => {
   const { cartItems } = useCart();
+
+  const [recommendations, setRecommendations] = useState<GalleryItem[]>([]);
+
+  useEffect(() => {
+    const fetchRecommendations = async () => {
+      try {
+        const data = await getShoppingBagRecommendations();
+        setRecommendations(data);
+      } catch (error) {
+        console.error("Failed to load recommendations", error);
+      }
+    };
+
+    fetchRecommendations();
+  }, []);
 
   if (cartItems.length === 0) {
     return (
@@ -24,12 +41,15 @@ const ShoppingBagPage: React.FC = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
         <div className="lg:col-span-2 bg-white p-6 rounded-lg shadow-md">
           {cartItems.map((item) => (
-            <BagItem key={item.id} item={item} />
+            <BagItem key={item.cartId} item={item} />
           ))}
         </div>
         <div className="lg:col-span-1">
           <OrderSummary />
         </div>
+      </div>
+      <div className="mt-16">
+        <RelatedProducts products={recommendations} contextType="all" />
       </div>
     </div>
   );
